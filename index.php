d302a4534ab81f3e30f4788347e57e00fcfd488a
Vincent Kim <84198755+vkimroyal@users.noreply.github.com>
2022-05-01 23:41:46 -0400

    CSS references fixed for Index Page styling

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styling/indexpage.css">
+    <link rel="stylesheet" href="styling/menu.css">
     <title>MAIV Property Hub</title>
 </head>
 <body>
