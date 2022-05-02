e0158b3cc2de4b7c8286a8ac3e32da8b992b2a67
arshad <arshadrizzzaman@gmail.com>
2022-05-01 20:58:39 -0400

    save

--- signup.php
+++ signup.php
@@ -13,61 +13,104 @@
     <?php
     require('db.php');
 
-    if (isset($_REQUEST['username'])){ //check if user is logged in
-    // Get data from sign up form
-    $username = $_REQUEST["username"];
-    $email = $_REQUEST["email"];
-    $role = $_REQUEST["role"];
-    $password = $_REQUEST["password"];
-    $encrypted_pwd = md5($password); // encrypt password using md5
+    if (isset($_REQUEST['username'])) { //check if user is logged in
+        // Get data from sign up form
+        $username = $_REQUEST["username"];
+        $email = $_REQUEST["email"];
+        $role = $_REQUEST["role"];
+        $password = $_REQUEST["password"];
+        $encrypted_pwd = md5($password); // encrypt password using md5
 
-    // write the sql query in php file to Insert the data into the table
+        // check if unique values are taken
 
-    $sql = "INSERT INTO users (username, email, role, password)
+        if (isset($_POST['username'])) {
+            $sql = "SELECT * FROM users WHERE username='$username'";
+            $results = $conn->query($sql);
+            if (mysqli_num_rows($results) > 0) {
+              echo "taken";	
+            }else{
+              echo 'not_taken';
+            }
+            exit();
+        }
+        if (isset($_POST['email'])) {
+            $sql = "SELECT * FROM users WHERE email='$email'";
+            $results = $conn->query($sql);
+            if (mysqli_num_rows($results) > 0) {
+              echo "taken";	
+            }else{
+              echo 'not_taken';
+            }
+            exit();
+        }
+
+        // check if email is already registered
+        $sql = "SELECT FROM users where email='$email'";
+        if ($conn->query($sql) === TRUE) {
+            // redirect user to login and display email already exists
+            header("Location: https://codd.cs.gsu.edu/~azaman3/WP/PW/4/login.php");
+        }
+
+        // write the sql query in php file to Insert the data into the table
+
+        $sql = "INSERT INTO users (username, email, role, password)
     VALUES('$username', '$email', '$role', '$encrypted_pwd')";
 
-    if ($conn->query($sql) === TRUE) {
-        header("Location: https://codd.cs.gsu.edu/~azaman3/WP/PW/4/login.php"); //change to your login page url
-    }} else { //change to your signup page url
+        if ($conn->query($sql) === TRUE) {
+            header("Location: https://codd.cs.gsu.edu/~azaman3/WP/PW/4/login.php"); //change to your login page url
+        }
+    } else { //change to your signup page url
     ?>
-    <!-- Form validation and page styling required -->
-    <div class="bg"></div>
-    <div class="container">
-        <form action="" method="POST">
-            <fieldset id="signup">
-                <center>
-                    <h1>Sign Up</h1>
-                    <p>Sign up for an account here.</p>
+        <!-- Form validation and page styling required -->
+        <script>
+            function Validate() {
+                var password = document.getElementById("password").value;
+                var confirmPassword = document.getElementById("confirm_password").value;
+                if (password != confirmPassword) {
+                    alert("Passwords do not match.");
+                    return false;
+                }
+                return true;
+            }
+        </script>
+        <div class="bg"></div>
+        <div class="container">
+            <form action="" method="POST">
+                <fieldset id="signup">
+                    <center>
+                        <h1>Sign Up</h1>
+                        <p>Sign up for an account here.</p>
 
-                    <hr>
+                        <hr>
 
-                    <label for="username"><b>Username</b></label><br>
-                    <input type="text" required name="username" placeholder="Username"><br>
+                        <label for="username"><b>Username</b></label><br>
+                        <input type="text" required name="username" placeholder="Username"><br>
 
-                    <label for="email"><b>Email</b></label><br>
-                    <input type="email" required name="email" placeholder="Email" /><br>
+                        <label for="email"><b>Email</b></label><br>
+                        <input type="email" required name="email" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" /><br>
 
-                    <label for="password"><b>Password</b></label><br>
-                    <input type="password" required name="password" placeholder="Password" /><br>
+                        <label for="password"><b>Password</b></label><br>
+                        <input type="password" required name="password" placeholder="Password" id="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" /><br>
 
-                    <label for="password"><b>Confirm Password</b></label><br>
-                    <input type="password" required name="confirm_password" placeholder="Confirm Password"><br>
+                        <label for="password"><b>Confirm Password</b></label><br>
+                        <input type="password" required name="confirm_password" placeholder="Confirm Password" id="confirm_password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" /><br>
 
-                    <label for="role"><b>Role: </b></label>
-                    <input type="radio" required name="role" value="buyer">Buyer
-                    <input type="radio" name="role" value="seller">Seller<br><br>
+                        <label for="role"><b>Role: </b></label>
+                        <input type="radio" required name="role" value="buyer">Buyer
+                        <input type="radio" name="role" value="seller">Seller<br><br>
 
-                    <input type="submit" value="Register">
+                        <input type="submit" value="Register" onclick="return Validate()">
 
-                    <br><br><hr>
-                    <p>
-                        Already have an account?<br>
-                        <a href="login.php">Log in here.</a>
-                    </p>
-                </center>
-            </fieldset>
-        </form>
-    </div>
+                        <br><br>
+                        <hr>
+                        <p>
+                            Already have an account?<br>
+                            <a href="login.php">Log in here.</a>
+                        </p>
+                    </center>
+                </fieldset>
+            </form>
+        </div>
     <?php } ?>
 </body>
 
